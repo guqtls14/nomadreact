410d8a44d02bd5e982272d0a275dd97c3611dba6
kyle-shk <guqtls85@naver.com>
2022-09-20 19:12:33 +0900

    3.2 circle props추가

--- src/Circle.tsx
+++ src/Circle.tsx
@@ -1,6 +1,10 @@
 import styled from "styled-components";
 
-const Container = styled.div<ContainerProps>``;
+const Container = styled.div<ContainerProps>`
+  width: 200px;
+  height: 200px;
+  background-color: ${(props) => props.bgColor};
+`;
 
 interface ContainerProps {
   bgColor: string;
