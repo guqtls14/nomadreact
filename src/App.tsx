07cccd7fca30c3ed1bb9dfb6a04100d1ee4071c4
kyle-shk <guqtls85@naver.com>
2022-09-20 21:20:42 +0900

    styled-components theme 타입스크립트 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,20 @@
 import { useState } from "react";
+import styled from "styled-components";
 
-import "./App.css";
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 
 function App() {
-  const [value, setValue] = useState("");
-
-  const onChange = (e: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = e; // e.currentTarget.value
-
-    setValue(value);
-  };
-
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-  };
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input
-          value={value}
-          onChange={onChange}
-          type="text"
-          placeholder="username"
-        />
-        <button>Log in</button>
-      </form>
-    </div>
+    <Container>
+      <H1>Title</H1>
+    </Container>
   );
 }
 
 export default App;
-
-// https://nomadcoders.co/react-masterclass/lectures/3336
-
-// ES6 문법이에요. event안 curentTarget안에 value의 값을 기존 이름 그대로 value 라는 변수를 만드는 거에요.
-
-// const value = event.currentTarget.value 랑 똑같습니다. 왜 저렇게 복잡하게 하냐고 물어보실수도 있는데 사실 저런식으로 한개만 만들때는 저 문법의 장점이 없어요.
-
-// 헌데 만약에 currentTarget안에서 value, tagName, width, id 이 4개를 가져오고 싶다고 하면 기존 문법으로는 이렇게 써야 되겠죠?
-
-// const value = event.currentTarget.value;
-// const tagName = event.currentTarget.tagName;
-// const width = event.currentTarget.width;
-// const id = event.currentTarget.id;
-
-// 이거를 이렇게 바꿔 쓰실수 있습니다.
-
-// const {
-// currentTarget: {value, tagName, width, id}
-// } = event;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme } from "./theme";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
-)
+);
