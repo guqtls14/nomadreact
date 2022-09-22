5236e3d504a7a1ba2bd9ff5da284174f70bc440e
kyle-shk <guqtls85@naver.com>
2022-09-22 21:05:44 +0900

    sytle: 스타일설정

--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,7 @@
 import { DefaultTheme } from "styled-components";
 
 export const theme: DefaultTheme = {
-  bgColor: "white",
-  textColor: "black",
-  btnColor: "tomato",
+  bgColor: "#9980FA",
+  textColor: "#009432",
+  accentColor: "#FFC312",
 };
