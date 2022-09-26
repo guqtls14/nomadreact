90934df247230ce9444ce4cdb83e0e4e29a289bb
kyle-shk <guqtls85@naver.com>
2022-09-26 15:23:22 +0900

    feat: Home styling & data fetching & route

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,11 +1,87 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components";
+
+const Container = styled.div`
+  padding: 0 20px;
+`;
+
+const Header = styled.header`
+  height: 10vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const CoinsList = styled.ul``;
+
+const Coin = styled.li`
+  background-color: white;
+  color: ${(props) => props.theme.bgColor};
+  margin-bottom: 10px;
+  padding: 20px;
+  border-radius: 15px;
+
+  a {
+    transition: color 0.2s ease-in;
+    display: block;
+  }
+
+  &:hover {
+    a {
+      color: ${(props) => props.theme.accentColor};
+    }
+  }
+`;
 
 const Title = styled.h1`
   color: ${(props) => props.theme.accentColor};
 `;
 
+const coins = [
+  {
+    id: "btc-bitcoin",
+    name: "Bitcoin",
+    symbol: "BTC",
+    rank: 1,
+    is_new: false,
+    is_active: true,
+    type: "coin",
+  },
+  {
+    id: "eth-ethereum",
+    name: "Ethereum",
+    symbol: "ETH",
+    rank: 2,
+    is_new: false,
+    is_active: true,
+    type: "coin",
+  },
+  {
+    id: "hex-hex",
+    name: "HEX",
+    symbol: "HEX",
+    rank: 3,
+    is_new: false,
+    is_active: true,
+    type: "token",
+  },
+];
+
 const Coins = () => {
-  return <Title>Coins</Title>;
+  return (
+    <Container>
+      <Header>
+        <Title>Coins</Title>
+      </Header>
+      <CoinsList>
+        {coins.map((item) => (
+          <Coin key={item.id}>
+            <Link to={`/${item.id}`}>{item.name} &rarr;</Link>
+          </Coin>
+        ))}
+      </CoinsList>
+    </Container>
+  );
 };
 
 export default Coins;
