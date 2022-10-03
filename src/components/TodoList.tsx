4bfa154d4f3d80b60c941a1abf158965c32b79a0
kyle-shk <guqtls85@naver.com>
2022-10-04 00:22:40 +0900

    selector2-1 filtering item 리팩토링전

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,40 +1,36 @@
-import { useRecoilValue } from "recoil";
+import { useRecoilState, useRecoilValue } from "recoil";
 
 import CreateToDo from "./CreateToDo";
 
-import { toDoSelector, toDoState } from "../atoms";
+import { toDoSelector, toDoState, categoryState } from "../atoms";
 
 import ToDo from "./ToDo";
+import React from "react";
 const TodoList = () => {
   // const toDos = useRecoilValue(toDoState);
   const [toDo, doing, done] = useRecoilValue(toDoSelector);
-
+  // 현재의value,수정하는함수
+  const [category, setCategory] = useRecoilState(categoryState);
+  // select value감지
+  const onInput = (e: React.FormEvent<HTMLSelectElement>) => {
+    setCategory(e.currentTarget.value);
+  };
   return (
     <div>
       <h1>To Dos</h1>
       <hr />
+      <select value={category} onInput={onInput}>
+        <option value="TO_DO">To Do</option>
+        <option value="DOING">Doing</option>
+        <option value="DONE">Done</option>
+      </select>
       <CreateToDo />
-      <h2>To Do</h2>
-      <ul>
-        {toDo.map((toDo) => (
-          <ToDo key={toDo.id} {...toDo} />
-        ))}
-      </ul>
-      <hr />
-      <h2>Doing</h2>
-      <ul>
-        {doing.map((toDo) => (
-          <ToDo key={toDo.id} {...toDo} />
-        ))}
-      </ul>
-      <hr />
-      <h2>Done</h2>
-      <ul>
-        {done.map((toDo) => (
-          <ToDo key={toDo.id} {...toDo} />
-        ))}
-      </ul>
-      <hr />
+      {category === "TO_DO" &&
+        toDo.map((aToDo) => <ToDo key={aToDo.id} {...aToDo} />)}
+      {category === "DOING" &&
+        doing.map((aToDo) => <ToDo key={aToDo.id} {...aToDo} />)}
+      {category === "DONE" &&
+        done.map((aToDo) => <ToDo key={aToDo.id} {...aToDo} />)}
     </div>
   );
 };
